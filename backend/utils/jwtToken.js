6cd45c375184ddbe4256b2f7c65651e88f6d7d58
Tapesh Sharma <114461563+tapesh1134@users.noreply.github.com>
2025-04-30 14:02:15 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,21 @@
 export const generateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonWebToken();
-    res
-      .status(statusCode)
-      .cookie("token", token, {
-        expires: new Date(
-          Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-      })
-      .json({
-        success: true,
-        message,
-        user,
-        token,
-      });
+  const token = user.generateJsonWebToken();
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Only send over HTTPS
+    sameSite: "Strict", // Prevent CSRF
   };
+
+  res
+    .status(statusCode)
+    .cookie("token", token, cookieOptions)
+    .json({
+      success: true,
+      message,
+      token
+    });
+};
